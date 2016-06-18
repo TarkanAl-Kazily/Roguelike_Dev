b4988b2637a9f944198f36ae989acf47110f1d6b
TarkanAl-Kazily <tarkucar@mac.com>
2016-06-18 12:51:10 -0700

    Added Screens which enable interactivity and change based on user input. ApplicationMain is modified to listen for key presses, and passes the event to the Screen.

--- rltarkan/ApplicationMain.java
+++ rltarkan/ApplicationMain.java
@@ -2,23 +2,48 @@ package rltarkan;
 
 import javax.swing.JFrame;
 import asciiPanel.AsciiPanel;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+import rltarkan.screens.Screen;
+import rltarkan.screens.StartScreen;
 
-public class ApplicationMain extends JFrame {
+public class ApplicationMain extends JFrame implements KeyListener
+{
     private static final long serialVersionUID = 1060623638149583738L;
 
     private AsciiPanel terminal;
+    private Screen screen;
 
-    public ApplicationMain(){
+    public ApplicationMain()
+    {
         super();
         terminal = new AsciiPanel();
-        terminal.write("The first parameter is how many characters", 10, 0);
-        terminal.write("in the horizontal direction", 10, 1);
-        terminal.write("And the second is how many characters in the vertical direction", 0, 10);
         add(terminal);
         pack();
+        screen = new StartScreen();
+        addKeyListener(this);
+        repaint();
     }
 
-    public static void main(String[] args) {
+    public void repaint()
+    {
+        terminal.clear();
+        screen.displayOutput(terminal);
+        super.repaint();
+    }
+
+    public void keyPressed(KeyEvent e)
+    {
+            screen = screen.respondToUserInput(e);
+            repaint();
+    }
+
+    public void keyReleased(KeyEvent e) { }
+
+    public void keyTyped(KeyEvent e) { }
+
+    public static void main(String[] args)
+    {
         ApplicationMain app = new ApplicationMain();
         app.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         app.setVisible(true);
---
+++ rltarkan/screens/Screen.java
@@ -0,0 +1,14 @@
+/*
+The displayOutput method takes an AsciiPanel to display itself on and the respondToUserInput takes the KeyEvent and can return the new screen. This way pressing a key can result in looking at a different screen.
+*/
+
+package rltarkan.screens;
+
+import java.awt.event.KeyEvent;
+import asciiPanel.AsciiPanel;
+
+public interface Screen {
+    public void displayOutput(AsciiPanel terminal);
+
+    public Screen respondToUserInput(KeyEvent key);
+}
